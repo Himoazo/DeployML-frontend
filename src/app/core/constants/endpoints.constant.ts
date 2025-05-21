a46505190e6c05706197c9c085404fc3803ec656
Hoomam <hoaz1600@student.miun.se>
2025-05-21 03:01:44 +0200

    api url

--- src/app/core/constants/endpoints.constant.ts
+++ src/app/core/constants/endpoints.constant.ts
@@ -1,3 +1,3 @@
-export const apiBaseUrl = "deployml-production.up.railway.app";
+export const apiBaseUrl = "https://deployml-production.up.railway.app";
 
 /* export const apiBaseUrl = "http://127.0.0.1:8000"; */
